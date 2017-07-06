9dcc4472a3e474d91ef5fd664395f33fef529c33
Marcelo Sales <masales@redhat.com>
2017-07-06 11:30:50 -0300

    Update index.php
    Add v1

--- index.php
+++ index.php
@@ -198,7 +198,7 @@ line-height: 1.4;
 
 <section class='container'>
           <hgroup>
-            <h1>PHP application on OpenShift! V1.0 Server: $_SERVER['SERVER_ADDR']</h1>
+            <h1>PHP application on OpenShift! V1 Server: $_SERVER['SERVER_ADDR']</h1>
           </hgroup>
 
 <?php
