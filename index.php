052216be51b293098f95c8103dbd660a14715685
Marcelo Sales <masales@redhat.com>
2017-07-10 16:28:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -199,9 +199,9 @@ line-height: 1.4;
 <section class='container'>
           <hgroup>
             <?php 
-                  echo "<h1>PHP application on OpenShift! V2</h1>";
+                  echo "<h1>PHP application on OpenShift! V3</h1>";
                   echo $_SERVER['SERVER_ADDR']
-            ?>
+            
           </hgroup>
 
 <?php
